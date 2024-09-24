af9ab007e254ca90a05fc75b0f5c348d5dd0bc6e
Kauã Fernandes <113432945+KauaFernandes1@users.noreply.github.com>
2024-09-24 17:30:26 -0300

    new

--- plataform-finance-manage/src/App.js
+++ plataform-finance-manage/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from './atomic/null/navbar-simple/navbar.js'
+
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      
     </div>
   );
 }
